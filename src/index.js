20b80e445a3f51db273c4b2ae50d16afc3f9f872
pengxu <pengxu19890115@gmail.com>
2019-11-12 15:30:31 -0800

    change root index file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Todo from "./component/Todo";
+import store from "./store";
+import { Provider } from "react-redux";
+
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Todo />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <Todo />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
