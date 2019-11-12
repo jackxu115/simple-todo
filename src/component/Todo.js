8bc9a15d81eb767412a492f95d2fd10467371686
pengxu <pengxu19890115@gmail.com>
2019-11-12 15:14:56 -0800

    Add the Todo file

---
+++ src/component/Todo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const Todo = ({}) => (
+  <div>
+    <input />
+    <button>Add Todo</button>
+  </div>
+);
+
+export default Todo;
